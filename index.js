d6aab9bf81bb6bb3612877b1630c2fefce33ba8f
Brendan Higgins <Bshannon@Briannas-MacBook-Air.local>
2019-04-07 17:25:13 -0400

    Setup server

---
+++ index.js
@@ -0,0 +1,28 @@
+// load all env variables from .env file into process.env object.
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+// const db = require('./queries');
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get('/', (_request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
+});
+// app.get('/users', db.getUsers);
+// app.get('/users/:id', db.getUserById);
+// app.post('/users', db.createUser);
+// app.put('/users/:id', db.updateUser);
+// app.delete('/users/:id', db.deleteUser);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
