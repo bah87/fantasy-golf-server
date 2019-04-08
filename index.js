34c95d5f379e571eb5fd3aef03d6149ef34485e9
Brendan Higgins <Bshannon@Briannas-MacBook-Air.local>
2019-04-07 22:34:17 -0400

    Update queries

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const db = require('./queries');
+const db = require('./queries');
 const app = express();
 
 app.use(bodyParser.json());
@@ -13,14 +13,9 @@ app.use(
   })
 );
 
-app.get('/', (_request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' });
-});
-// app.get('/users', db.getUsers);
-// app.get('/users/:id', db.getUserById);
-// app.post('/users', db.createUser);
-// app.put('/users/:id', db.updateUser);
-// app.delete('/users/:id', db.deleteUser);
+app.get('/', db.getHome);
+app.get('/salaries', db.getSalaries);
+app.post('/salaries', db.createSalary);
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}.`);
---
+++ queries.js
@@ -0,0 +1,42 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+const getHome = (_request, response) => {
+  response.json({ info: 'test getHome ...' });
+};
+
+const getSalaries = (_request, response) => {
+  client.query('SELECT * FROM salaries;', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createSalary = (request, response) => {
+  const { playerId, salary } = request.body;
+
+  client.query(
+    'INSERT INTO salaries (player_id, salary) VALUES ($1, $2)',
+    [playerId, salary],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`Salary added with ID: ${results.insertId}`);
+    }
+  );
+};
+
+module.exports = {
+  getHome,
+  getSalaries,
+  createSalary
+};
