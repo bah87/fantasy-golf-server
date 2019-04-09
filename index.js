b6a11c2bcc9ef9b131fd259c59ddd56338e0e8af
Brendan Higgins <Bshannon@Briannas-MacBook-Air.local>
2019-04-09 09:59:30 -0400

    Change request header

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ app.use(
 // Add headers
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://fantasy-golf-app.herokuapp.com/'
+  );
 
   // Request methods you wish to allow
   res.setHeader(
