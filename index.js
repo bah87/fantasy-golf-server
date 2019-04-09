5e49dfe08a0aec1297d6d0addd98816b072a5ddb
Brendan Higgins <Bshannon@Briannas-MacBook-Air.local>
2019-04-09 09:16:34 -0400

    Disable cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const db = require('./queries');
 const app = express();
 
@@ -13,36 +14,44 @@ app.use(
   })
 );
 // Add headers
-app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+// app.use(function(req, res, next) {
+//   // Disable cors
+//   res.setHeader(false);
 
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
+//   // Website you wish to allow to connect
+//   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
+//   // Request methods you wish to allow
+//   // res.setHeader(
+//   //   'Access-Control-Allow-Methods',
+//   //   'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//   // );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Request headers you wish to allow
+//   // res.setHeader(
+//   //   'Access-Control-Allow-Headers',
+//   //   'X-Requested-With,content-type'
+//   // );
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   // res.setHeader('Access-Control-Allow-Credentials', true);
 
-app.get('/', db.getHome);
-app.get('/salaries', db.getSalaries);
-app.post('/salaries', db.createSalary);
-app.delete('/salaries', db.deleteSalaries);
-app.post('/teams', db.createTeam);
-app.get('/teams', db.getTeams);
+//   // Pass to next layer of middleware
+//   next();
+// });
+
+const corsOptionsDelegate = (_req, callback) => {
+  // disable CORS
+  callback(null, { origin: false });
+};
+
+app.get('/', cors(corsOptionsDelegate), db.getHome);
+app.get('/salaries', cors(corsOptionsDelegate), db.getSalaries);
+app.post('/salaries', cors(corsOptionsDelegate), db.createSalary);
+app.delete('/salaries', cors(corsOptionsDelegate), db.deleteSalaries);
+app.post('/teams', cors(corsOptionsDelegate), db.createTeam);
+app.get('/teams', cors(corsOptionsDelegate), db.getTeams);
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}.`);
